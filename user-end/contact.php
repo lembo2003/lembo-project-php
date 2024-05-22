4178f5bf6a00e6693e6a9c204f251351d0e0eec8
lembo2003 <lembo0909@gmail.com>
2024-05-23 04:28:33 +0700

    Update contact.php

--- user-end/contact.php
+++ user-end/contact.php
@@ -1,3 +1,6 @@
+<?php 
+  include('../controls.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -137,31 +140,48 @@
                 <div class="row">
                   <div class="col-lg-12 col-md-12 col-sm-12">
                     <fieldset>
-                      <input name="name" type="text" class="form-control" id="name" placeholder="Full Name" required="">
+                      <input name="txt_fullname" type="text" class="form-control" id="name" placeholder="Full Name" required="">
                     </fieldset>
                   </div>
                   <div class="col-lg-12 col-md-12 col-sm-12">
                     <fieldset>
-                      <input name="email" type="text" class="form-control" id="email" placeholder="E-Mail Address" required="">
+                      <input name="txt_email" type="text" class="form-control" id="email" placeholder="E-Mail Address" required="">
                     </fieldset>
                   </div>
                   <div class="col-lg-12 col-md-12 col-sm-12">
                     <fieldset>
-                      <input name="subject" type="text" class="form-control" id="subject" placeholder="Subject" required="">
+                      <input name="txt_phonenumber" type="text" class="form-control" id="subject" placeholder="Phone" required="">
                     </fieldset>
                   </div>
                   <div class="col-lg-12">
                     <fieldset>
-                      <textarea name="message" rows="6" class="form-control" id="message" placeholder="Your Message" required=""></textarea>
+                      <textarea name="txt_message" rows="6" class="form-control" id="message" placeholder="Your Message" required=""></textarea>
                     </fieldset>
                   </div>
                   <div class="col-lg-12">
                     <fieldset>
-                      <button type="submit" id="form-submit" class="filled-button">Send Message</button>
+                      <button name="btn_submit" type="submit" id="form-submit" class="filled-button">Send Message</button>
                     </fieldset>
                   </div>
                 </div>
               </form>
+              <?php
+
+              if(isset($_POST["btn_submit"])) {
+                $tbl_contact = new tbl_contact();
+                if($tbl_contact->insert_contact(
+                  $_POST["txt_fullname"], 
+                  $_POST["txt_email"], 
+                  $_POST["txt_phonenumber"], 
+                  $_POST["txt_message"])) 
+                {
+                  echo "<script> alert('gửi contact thành công') </script>";
+                }
+                else {
+                  echo "<script> alert('Lỗi') </script>";
+                }
+              }             
+            ?>
             </div>
           </div>
           <div class="col-md-4">
